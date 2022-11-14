5f797c03618f8112954008a6f4c797f01295fef1
gmdep003 <95266030+gmdep003@users.noreply.github.com>
2022-11-14 15:07:30 -0500

    Help Page xml

---
+++ app/src/main/java/com/example/myapplication/HelpPage.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+class HelpPage : Fragment() {
+    /** Binding to XML layout */
+    private lateinit var binding: HelpPageBinding
+
+}
