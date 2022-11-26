dda841898c92e0bbcf90b29f8e1cb86bf9cbe369
gmdep003 <95266030+gmdep003@users.noreply.github.com>
2022-11-26 12:13:34 -0500

    help page

---
+++ app/src/main/java/root/localApparel/HelpPage.java
@@ -0,0 +1,15 @@
+package root.localApparel;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpPage extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_page);
+    }
+}
+
+
