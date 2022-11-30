39a0e9c9a6a51416660e4c14a0245cf0d277c9b8
asd333333 <anjali.csd88@gmail.com>
2022-11-30 18:58:39 -0500

    prof page

--- app/src/main/java/root/localApparel/ProfileFragment.java
+++ app/src/main/java/root/localApparel/ProfileFragment.java
@@ -3,12 +3,23 @@ package root.localApparel;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +32,11 @@ public class ProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
+    TextView name, email, password;
+    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -52,42 +68,51 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        Button btn = (Button) findViewbyId(R.id.prof_pastorders);
-//
-//        btn.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Users");
+
+        email = view.findViewById(R.id.register_email);
+        name = view.findViewById(R.id.register_name);
+        password = view.findViewById(R.id.register_password);
+        String uid = firebaseUser.getUid();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    // Retrieving Data from firebase
+                    if (snapshot1.child("uid").getValue().equals(uid)) {
+                        String name1 = "" + snapshot1.child("name").getValue().toString();
+                        String email1 = "" + snapshot1.child("email").getValue().toString();
+
+
+                        name.setText(name1);
+                        email.setText(email1);
+                    }
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+//        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(View view) {
-//                openSellingCloset();
+//            public void onClick(View v) {
+//                startActivity(new Intent(getActivity(), EditProfilePage.class));
 //            }
-//
-//
 //        });
-
+        return inflater.inflate(R.layout.fragment_profile, container, false);
 
     }
-
-//    private Object findViewbyId(int prof_pastorders) {
-//    }
-//
-//    public void openSellingCloset() {
-//        Intent intent = new Intent(this, SellingClosetFragment.class);
-//        startActivity(intent);
-//
-//
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//
-//
-//        private Object findViewbyId ( int prof_pastorders){
-//        }
-//
-        @Override
-        public View onCreateView (LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState){
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_profile, container, false);
-        }
-    }
-//}
+}
