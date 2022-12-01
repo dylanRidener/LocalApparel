cfa95fe5324a28133bac5f532dbbb0a523aa24db
dylanRidener <107432785+dylanRidener@users.noreply.github.com>
2022-11-30 20:17:10 -0500

    temp

--- app/src/main/java/root/localApparel/HelpPage.java
+++ app/src/main/java/root/localApparel/HelpPage.java
@@ -6,13 +6,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HelpPage extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.help_page, container, false);
+
         // Inflate the layout for this fragment
+        Button store = view.findViewById(R.id.SubmitButton);
+        EditText editText = view.findViewById(R.id.helper_comment);
+        store.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.getText().clear();
+                editText.setText(" ");
+            }
+        });
         return inflater.inflate(R.layout.help_page, container, false);
     }
 }
