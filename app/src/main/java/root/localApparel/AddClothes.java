400a213a83d5a5515764477674fdf0320ac0308a
gmdep003 <95266030+gmdep003@users.noreply.github.com>
2022-11-30 23:18:05 -0500

    post buttons move back to closet page

--- app/src/main/java/root/localApparel/AddClothes.java
+++ app/src/main/java/root/localApparel/AddClothes.java
@@ -8,19 +8,43 @@ import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AddClothes extends Fragment {
     public AddClothes() {
         // Required empty public constructor
     }
 
-    Button post;
+    Button postRent;
+    Button postBuy;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.clothing_item, container, false);
+
+        postRent = view.findViewById(R.id.PostRent);
+        postRent.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Fragment addFrag = new SellingClosetFragment();
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.content, addFrag).commit();
+            }
+        });
+
+        postBuy = view.findViewById(R.id.PostBuy);
+        postBuy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Fragment addFrag = new SellingClosetFragment();
+                FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.content, addFrag).commit();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.clothing_item, container, false);
+        return view;
     }
 
 }
