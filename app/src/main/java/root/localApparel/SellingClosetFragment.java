ac93d81e43bd1912f785299b3e85c8bde568e006
dylanRidener <107432785+dylanRidener@users.noreply.github.com>
2022-12-04 02:11:00 -0500

    fixing up closet

--- app/src/main/java/root/localApparel/SellingClosetFragment.java
+++ app/src/main/java/root/localApparel/SellingClosetFragment.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,22 +16,44 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SellingClosetFragment extends Fragment {
 
+    FirebaseAuth firebaseAuth;
+    String myuid;
+    RecyclerView recyclerView;
+    List<PostObjects> posts;
+    PostHolder postHolder;
+
+    public SellingClosetFragment() {
+        // Required empty public constructor
+    }
     Button addBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_selling_closet, container, false);
-
+        firebaseAuth = FirebaseAuth.getInstance();
+        recyclerView = view.findViewById(R.id.postrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        myuid = firebaseAuth.getInstance().getUid();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        posts = new ArrayList<>();
+        loadPosts();
         addBtn = view.findViewById(R.id.AddButton);
         addBtn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -43,39 +67,33 @@ public class SellingClosetFragment extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
+    private void loadPosts() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Posts");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                posts.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    if (dataSnapshot1.child("uid").getValue() != null && dataSnapshot1.child("uid").getValue().equals(myuid)) {
+                        PostObjects modelPost = dataSnapshot1.getValue(PostObjects.class);
+                        posts.add(modelPost);
+                    }
+                }
+                postHolder = new PostHolder(getActivity(), posts);
+                recyclerView.setAdapter(postHolder);
+            }
 
-//    private void loadMyPosts() {
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-//        layoutManager.setReverseLayout(true);
-//        layoutManager.setStackFromEnd(true);
-//        postrecycle.setLayoutManager(layoutManager);
-//
-//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Posts");
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                posts.clear();
-//                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-//                    for (DataSnapshot childsnapshot: dataSnapshot1.getChildren()) {
-//                        if (childsnapshot.child("uid").getValue().equals(uid)) {
-//                            ModelPosts modelPost = childsnapshot.getValue(ModelPosts.class);
-//                            posts.add(modelPost);
-//                        }
-//
-//                    }
-//                }
-//                adapterPosts = new AdapterPosts(getActivity(), posts);
-//                postrecycle.setAdapter(adapterPosts);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                Toast.makeText(getActivity(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
-//            }
-//        });
-//    }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+                Toast.makeText(getActivity(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
 }
